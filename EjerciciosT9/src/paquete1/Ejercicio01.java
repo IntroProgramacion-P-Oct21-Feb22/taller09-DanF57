2b12001386e0f26973737c685e78d1f0e7d2a6ce
Daniel Eduardo Flores <danser2002@hotmail.com>
2021-12-17 11:15:24 -0500

    Ejercicio01

--- EjerciciosT9/src/paquete1/Ejercicio01.java
+++ EjerciciosT9/src/paquete1/Ejercicio01.java
@@ -5,9 +5,11 @@
  */
 package paquete1;
 
+//Catalogar rangos
+
 /**
  *
- * @author reroes
+ * @author Daniel
  */
 public class Ejercicio01 {
 
@@ -19,27 +21,29 @@ public class Ejercicio01 {
         String[] rangos =       {"1-5", "6-10", "11-15", "16-20"};
         int[] valoresRangos =   {10,      20,     3,        1}    
         */
+        
         int[] respuestas = {1, 10, 11, 12, 12, 13, 16, 17, 18, 8, 9, 10, 19, 20};
+        
         String[] rangos = {"1-5", "6-10", "11-15", "16-20"};
-        int[] valoresRangos = new int[4]; // {0, 0, 0, 0}
+        int[] valoresRangos = new int[4]; // {0, 0, 0, 0} //{1, 4, 4, 5}
         int respuesta;
         
         for (int i = 0; i < respuestas.length; i++) { 
-            respuesta = respuestas[i]; 
-                    
-            if ((respuesta >= 1) || (respuesta <=5)){
+            respuesta = respuestas[i]; //1 
+                    //RECORDAR USAR AND Y OR CORRECTAMENTE
+                    //1>=1 v     || 1<=5
+            if ((respuesta >= 1) && (respuesta <=5)){
                 valoresRangos[0] = valoresRangos[0] + 1;
-                
             }else{
                     
-                if ((respuesta >= 6) || (respuesta <=10)){
+                if ((respuesta >= 6) && (respuesta <=10)){
                     valoresRangos[1] = valoresRangos[1] + 1;
                 }else{
                             
-                    if ((respuesta >= 11) || (respuesta <=15)){
+                    if ((respuesta >= 11) && (respuesta <=15)){
                         valoresRangos[2] = valoresRangos[2] + 1; //3+1 
                     }else{
-                        if ((respuesta >= 16) || (respuesta <=20)){
+                        if ((respuesta >= 16) && (respuesta <=20)){
                             valoresRangos[3] = valoresRangos[3] + 1;//1+1
                         }
                     
